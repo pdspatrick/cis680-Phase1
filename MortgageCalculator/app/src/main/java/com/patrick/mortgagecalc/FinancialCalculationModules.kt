6e276f8df3e0b2a2b8f75dadeb881c2141fbf93c
Patrick Flynn <pdspatrick@gmail.com>
2021-01-15 11:48:56 -0600

    initial definition of mortgage object and related functions

---
+++ MortgageCalculator/app/src/main/java/com/patrick/mortgagecalc/FinancialCalculationModules.kt
@@ -0,0 +1,41 @@
+package com.patrick.mortgagecalc
+
+    fun amTable(){
+
+    }
+
+    fun calcPayment(){
+
+    }
+
+    fun calcInterest(){
+
+    }
+
+    fun calcPrincipalPayment(){
+
+    }
+
+    fun compareMortgage(){
+
+    }
+
+class Mortgage {
+    var length: Int = 0
+    var remainLength: Int = 0
+    var custName: String = ""
+    var initialBalance: Number = 0
+    var currentBalance: Number = 0
+    private var downpay: Number = 0
+    private var downpercent: Number = 0
+    var interestRate: Number = 0
+
+    fun setDownPayment(payment: Number){
+
+        }
+    }
+
+
+fun store(aThing: Mortgage){
+
+}
